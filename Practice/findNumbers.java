9c92630dabf5466cab9566755a8fe526abe110cd
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2021-08-25 00:24:12 +0530

    1295

---
+++ Practice/findNumbers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num: nums){
+            int i=0, rem=0;
+            while(num>0){
+                i+=1;
+                num=num/10;
+            }
+            if(i%2==0)count+=1;
+        }
+        return count;
+    }
+}
