3636e151c572f091aa2eed33eaa402197c82541e
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2021-08-28 20:25:45 +0530

    GFG

--- Practice/findElement_in_InfiniteArray.java
+++ Practice/findElement_in_InfiniteArray.java
@@ -11,17 +11,17 @@ public class Main
 	public static void main(String[] args) {
 		int [] arr = {5,7,8,9,10,11,12,14,25,27,33,35,55,67,777,7543,66666,99999};
 		int target = 55;
-    ceil(arr,target);		
+    		findElement_in_InfiniteArray(arr,target);		
 	}
 	
-	public static void ceil(int[] arr, int target){
+	public static void findElement_in_InfiniteArray(int[] arr, int target){
 	    int start,end,x;
 	    start =0;end=1;
 	    
-      //OR while(target>arr[end])
+      	    //OR while(target>arr[end])
 	    while(true){
 	        if(target>=arr[start] && target<=arr[end]){
-	            x=bin(arr,start,end,target);
+	            x=binSearch(arr,start,end,target);
 	            System.out.println(x);
 	            break;
 	        }
@@ -34,14 +34,14 @@ public class Main
 	    }
 	}
 	
-	public static int bin(int[] nums, int start, int end, int target){
-        while(start<=end){
-            int mid = start + ((end - start)/2);
-            if(target < nums[mid]) end = mid-1;
-            else if(target > nums[mid]) start = mid+1;
-            else return mid;
-        }
-        
-        return -1;
+	public static int binSearch(int[] nums, int start, int end, int target){
+		while(start<=end){
+		    int mid = start + ((end - start)/2);
+		    if(target < nums[mid]) end = mid-1;
+		    else if(target > nums[mid]) start = mid+1;
+		    else return mid;
+		}
+
+		return -1;
 	}
 }
