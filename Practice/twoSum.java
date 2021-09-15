a192b74a2b63f18f9331bcc9cea1ea89fa62506e
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2021-09-15 19:39:20 +0530

    1

---
+++ Practice/twoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                ans[0]=map.get(target-nums[i]);
+                ans[1]=i;
+                return ans;
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return ans;
+    }
+}
