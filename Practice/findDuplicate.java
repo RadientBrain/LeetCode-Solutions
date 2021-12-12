82febacaec2668d4ceaf7b0715099b02df57b006
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2021-12-12 13:25:19 +0530

    287

---
+++ Practice/findDuplicate.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]<nums.length && nums[i]!=nums[correct]){
+                swap(nums, i, correct);
+            }else{
+                i++;
+            }
+        }
+        
+        for(i=0;i<nums.length;i++){
+            if(i != nums[i]-1){
+                return nums[i];
+            }
+        }
+        
+        return -1;
+    }
+    
+    void swap(int[] nums, int first, int second){
+        int temp=0;
+        temp = nums[first];
+        nums[first]=nums[second];
+        nums[second]=temp;
+    }
+}
