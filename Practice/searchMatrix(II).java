921a65e41555d58f389e23fc442ad2325eee6cc3
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2021-12-20 21:32:02 +0530

    240

---
+++ Practice/searchMatrix(II).java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+    int i = matrix.length - 1;
+    int j = 0;
+    
+    while(i > -1 && j < matrix[0].length) {
+        
+        if(matrix[i][j] == target) {
+            return true;
+        }
+        else if(matrix[i][j] > target) {
+            i--;
+        }
+        else {
+            j++;
+        }
+    }
+    return false;
+    }
+}
