4c03ed8b514d72ce224b966c53e0eca478c1931d
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2021-12-21 21:01:52 +0530

    448

---
+++ Practice/findDisappearedNumbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+        int i=0;
+        while(i<nums.size()){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums[i],nums[correct]);
+            }else{
+                i++;
+            }
+        }
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            if(i!=nums[i]-1) res.push_back(i+1);
+        }
+        return res;
+    }
+};
